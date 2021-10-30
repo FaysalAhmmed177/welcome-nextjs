69e006c3f8def5dacd8acbafdfbc209e6f3a1669
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-10-30 23:20:08 +0600

    created a next js app

---
+++ pages/news/index.js
@@ -0,0 +1,10 @@
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h1>The news page</h1>
+        </div>
+    );
+};
+
+export default NewsPage;
---
+++ pages/news/something-important.js
@@ -0,0 +1,9 @@
+const DetailPage = () => {
+    return (
+        <div>
+            <h1>The detail page</h1>
+        </div>
+    );
+};
+
+export default DetailPage;
